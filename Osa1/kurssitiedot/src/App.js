e896ba90f6a81e9491f1645fa9cfd54f930a78d0
samijuvaste <sami@juvaste.fi>
2022-08-10 18:19:59 +0300

    Added Osa1

---
+++ Osa1/kurssitiedot/src/App.js
@@ -0,0 +1,60 @@
+const Header = (props) => {
+  return (
+      <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+      <p>
+        {props.text} {props.count}
+      </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part text={props.parts[0].name} count={props.parts[0].exercises} />
+      <Part text={props.parts[1].name} count={props.parts[1].exercises} />
+      <Part text={props.parts[2].name} count={props.parts[2].exercises} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+      <p>Number of exercises {props.parts[0].exercises
+        + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
