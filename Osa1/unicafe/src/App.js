abe34f660db28f891e4a331c3e0b27e54f742b44
samijuvaste <sami@juvaste.fi>
2022-08-11 22:30:50 +0300

    deleted comment

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -41,7 +41,7 @@ const Statistics = ({good, neutral, bad}) => {
 }
 
 const App = () => {
-  // tallenna napit omaan tilaansa
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
