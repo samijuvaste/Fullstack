82b517e9524e11d7b9ea55c151900ef85c68dddc
samijuvaste <sami@juvaste.fi>
2023-06-12 20:20:49 +0300

    Exercise 9.22

--- Part9/patientorBackend/src/types.ts
+++ Part9/patientorBackend/src/types.ts
@@ -1,4 +1,4 @@
-export interface Diagnose {
+export interface Diagnosis {
     code: string;
     name: string;
     latin?: string;
@@ -10,9 +10,51 @@ export enum Gender {
     Other = 'other'
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+    'Healthy' = 0,
+    'LowRisk' = 1,
+    'HighRisk' = 2,
+    'CritcalRisk' = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: 'HealthCheck';
+    healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: 'Hospital';
+    discharge: Discharge;
+}
+
+interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
     id: string;
